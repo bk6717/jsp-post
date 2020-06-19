bb53a2834980b729191b739e666406b8689aa23e
bk <bk@naver.com>
2020-06-19 12:42:36 +0900

    11.글목록 보기

--- src/com/cos/apple/model/Post.java
+++ src/com/cos/apple/model/Post.java
@@ -1,10 +1,17 @@
 package com.cos.apple.model;
 
-import com.sun.jmx.snmp.Timestamp;
 
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Post {
 	private int id;
 	private int memberid;
